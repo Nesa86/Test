22e0ec217204c6c5f4269ca65bc0f1d3d22fd6f2
unknown <nenaddikic86@gmail.comgit config --global user.name Nesa86>
2019-04-15 20:30:54 +0200

    Change

--- src/nenad/dikic/Main.java
+++ src/nenad/dikic/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("June was here!");
 		System.out.println("Andrija was here!");
-		
+		System.out.println("Nesa was here!");
+
 	}
 
 }
